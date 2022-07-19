31b8e728bd5e8a40da93f28eac25bd4bf250022a
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2022-07-19 22:14:44 +0530

    Meta changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,27 +60,22 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Siddhesh Shinde.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">Developer | ML Enthusiast</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
+        I'm a B.Tech CSE Student specializing in building exceptional, high-quality web applications
+        and flutter applications. Besides that I love working on open source software projects and
+        sharpening my problem solving skills by competitive programming.
+        {/* <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
-        </a>
-        .
+        </a> */}
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="/resume.pdf" target="_blank" rel="noreferrer">
+      Resume
     </a>
   );
 
